78d0254f8cfd8e4723d43fa6a2de8ad99f82337b
n <gayetni@gmail.com>
2023-11-11 15:16:31 +0100

    splatting

--- textureSplatting/main.js
+++ textureSplatting/main.js
@@ -1,96 +1,93 @@
-import "../js-lib/dom/styles/styles.js"
+'use strict'
 
-const canvas = document.createElement('canvas')
-canvas.style.top = '0'
-canvas.style.left = '0'
-canvas.style.width = '100%'
-canvas.style.height = '100%'
-canvas.style.zIndex = '-1'
-canvas.style.position = 'fixed'
+import { styles } from "../js-lib/dom/styles/styles.js"
+import { OrbitControls } from "../js-lib/3dEngine/controls/OrbitControls.js"
+import { LightParticleObject } from "../js-lib/3dEngine/extras/LightParticleObject.js"
+import { loadGLTF } from "../js-lib/3dEngine/loaders/gltfLoader.js"
+import { Renderer } from "../js-lib/3dEngine/renderer/Renderer.js"
+import { PointLight } from "../js-lib/3dEngine/sceneGraph/light/PointLight.js"
+import { loopRaf } from "../js-lib/globals/loopRaf.js"
+import { SplattingNode } from "../js-lib/3dEngine/sceneGraph/gltf/splatting/SplattingNode.js"
+import { SplattingTextures } from "../js-lib/3dEngine/sceneGraph/gltf/splatting/SplattingTextures.js"
+import { getImage } from "../js-lib/utils/utils.js"
+import { Vector3 } from "../js-lib/math/Vector3.js"
+import { Vector2 } from "../js-lib/math/Vector2.js"
 
-const gl = canvas.getContext("webgl2", {
-    alpha: false,
-    antialias: false,
-    depth: true,
-    // desynchronized?: boolean
-    failIfMajorPerformanceCaveat: false,
-    powerPreference: 'default',
-    premultipliedAlpha: true,
-    preserveDrawingBuffer: false,
-    stencil: true
+const renderer = new Renderer()
+document.body.prepend(renderer.domElement)
+renderer.camera.far = 1000
+
+// Point Light
+const lightParticleObject = new LightParticleObject()
+lightParticleObject.count = 2
+renderer.scene.objects.add(lightParticleObject)
+
+const pointLight1 = new PointLight()
+pointLight1.color.setRGB(1, 0, 0)
+pointLight1.position.set(3, 0, 0)
+pointLight1.intensity = 5
+renderer.pointLights.add(pointLight1)
+const pointLight2 = new PointLight()
+pointLight2.color.setRGB(0, 0, 1)
+pointLight2.position.set(-3, 0, 0)
+renderer.pointLights.add(pointLight2)
+
+const orbitControls = new OrbitControls(renderer.camera, renderer.domElement)
+
+// Mesh Init
+
+const gltfNodes = await loadGLTF(new URL('./zone0/zone0.glb', import.meta.url))
+
+const gltfNode = gltfNodes['terrainStart']
+
+const [
+    splattingImage,
+    image1,
+    normalImage1,
+    image2,
+    normalImage2,
+    image3,
+    normalImage3,
+    image4,
+    normalImage4
+] = await Promise.all([
+    getImage(new URL('./zone0/textureSplatting.png', import.meta.url).href),
+
+    getImage(new URL('./zone0/textures/Grass001_1K-JPG/Grass001_1K_Color.jpg', import.meta.url).href),
+    getImage(new URL('./zone0/textures/Grass001_1K-JPG/Grass001_1K_NormalGL.jpg', import.meta.url).href),
+
+    getImage(new URL('./zone0/textures/Ground037_1K-JPG/Ground037_1K_Color.jpg', import.meta.url).href),
+    getImage(new URL('./zone0/textures/Ground037_1K-JPG/Ground037_1K_NormalGL.jpg', import.meta.url).href),
+
+    getImage(new URL('./zone0/textures/Ground031_1K-JPG/Ground031_1K_Color.jpg', import.meta.url).href),
+    getImage(new URL('./zone0/textures/Ground031_1K-JPG/Ground031_1K_NormalGL.jpg', import.meta.url).href),
+
+    getImage(new URL('./zone0/textures/Ground054_1K-JPG/Ground054_1K_Color.jpg', import.meta.url).href),
+    getImage(new URL('./zone0/textures/Ground054_1K-JPG/Ground054_1K_NormalGL.jpg', import.meta.url).href),
+])
+
+const splattingTextures = new SplattingTextures({
+    splattingImage,
+    image1,
+    normalImage1,
+    map1Scale: new Vector2(100, 100),
+    image2,
+    normalImage2,
+    map2Scale: new Vector2(100, 100),
+    image3,
+    normalImage3,
+    map3Scale: new Vector2(100, 100),
+    image4,
+    normalImage4,
+    map4Scale: new Vector2(100, 100),
 })
 
+const node = new SplattingNode(gltfNode, splattingTextures)
+renderer.scene.addNode3D(node)
 
+// Animation
 
-this.gltfNodes = await loadGLTF(new URL('./zone0.glb', import.meta.url))
-
-const node = gltfNodes['terrainStart']
-
-{
-    gl.getUniformLocation(glProgram, "position")
-
-
-    this.splattingTexture = new Texture({})
-    this.splattingTexture.data.src = new URL('./textureSplatting.png', import.meta.url).href
-    this.map1 = new Texture({
-        minFilter: 'LINEAR_MIPMAP_NEAREST',
-        magFilter: 'LINEAR',
-        wrapS: 'REPEAT', wrapT: 'REPEAT',
-        scale: new Vector2(60, 60)
-    })
-
-
-    this.map1.data.src = new URL('./textures/Grass001_1K-JPG/Grass001_1K_Color.jpg', import.meta.url).href
-
-    this.map2 = new Texture({
-        minFilter: 'LINEAR_MIPMAP_NEAREST',
-        wrapS: 'REPEAT', wrapT: 'REPEAT', scale: new Vector2(40, 40)
-    })
-    this.map2.data.src = new URL('./textures/Ground037_1K-JPG/Ground037_1K_Color.jpg', import.meta.url).href
-
-    this.map3 = new Texture({
-        minFilter: 'LINEAR_MIPMAP_NEAREST',
-        wrapS: 'REPEAT', wrapT: 'REPEAT', scale: new Vector2(5, 5)
-    })
-    this.map3.data.src = new URL('./textures/Ground031_1K-JPG/Ground031_1K_Color.jpg', import.meta.url).href
-
-    this.gltfPrimitive = node.mesh.primitives[0]
-}
-{
-    rockGltfPrimitive = gltfNodes['rock1'].mesh.primitives[0]
-    const texture = new Texture({})
-    texture.data.src = new URL('./textures/Rock037_1K-JPG/Rock037_1K_Color.jpg', import.meta.url).href
-    rockGltfPrimitive.material.textures['u_map'] = texture
-}
-
-
-
-export class Zone0 {
-    static init = init
-    static destroy = destroy
-
-    #meshTerrain = new SplattingMesh(
-        gltfPrimitive,
-        splattingTexture,
-        map1, map2, map3,
-    )
-    #rock1 = new GltfMesh(rockGltfPrimitive, 'rock1')
-
-    node3D = new Node3D({
-        objects: [this.#meshTerrain, this.#rock1]
-    })
-
-    #lightsDispose
-
-    constructor(renderer) {
-        if (!gltfPrimitive) throw new Error('TerrainNode is not initialized "await TerrainNode.init()"')
-        initLights(renderer)
-        // this.#lightsDispose = initPointLights()
-    }
-
-    dispose() {
-        this.#lightsDispose()
-        this.node3D.dispose()
-    }
-}
-
+loopRaf.listeners.add(() => {
+    orbitControls.update()
+    renderer.render(loopRaf.deltatimeSecond)
+})
